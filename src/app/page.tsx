a74b55b490f8e0bbbd0befb43eeec70e4dfc14fc
Temur <shernazarovtemurcom001@gmail.com>
2024-03-01 23:33:18 +0500

    added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,22 @@ import React from "react";
 
 const HomePage = () => {
   return (
-    <div>
-      <h1 className="text-3xl">HomePage</h1>
+    <div className="container mx-auto mt-28   xl:pl-20 xl:pr-20 md:pl-2 md:pr-2 pl-2 pr-2 ">
+      <div className="flex flex-wrap justify-between items-baseline">
+        <input
+          type="search"
+          placeholder="Searching..."
+          className="w-80  md:w-96 p-4 rounded-lg"
+        />
+        <select name="all" id="all" className="mt-5 p-3 rounded-lg">
+          <option value="all">Filter by Region</option>
+          <option value="africa">Africa</option>
+          <option value="america">America</option>
+          <option value="asia">Asia</option>
+          <option value="europe">Europe</option>
+          <option value="oceania">Oceania</option>
+        </select>
+      </div>
     </div>
   );
 };
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,12 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
+    // dark:bg-slate-900 dark:text-white
+    <div className=" bg-white   opacity-95 pt-5 pb-5 top-0 left-0 right-0  fixed shadow-lg shadow-black-500/50  xl:pl-20 xl:pr-20 md:pl-2 md:pr-2 pl-2 pr-2  ">
+      <div className="container  mx-auto flex items-center   justify-between">
+        <p className="leading-8 font-extrabold text-lg ">Where in the world?</p>
+        <p>Dark Mode</p>
+      </div>
     </div>
   );
 };
